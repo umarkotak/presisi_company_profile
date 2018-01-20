b4c3c5ef860e22c1391e2f1af52672270c16f04d
malikkhoiri <abdulchoiri19@gmail.com>
2018-01-20 22:00:04 +0700

    edit company page

--- pages/company.php
+++ pages/company.php
@@ -42,7 +42,7 @@ function getDescription($conn, $title){
 <section class="container p-center fade-hidden" id="anchor-slide">
     <div class="row">
         <div class="col-xs-12 m-t-50 t-center">
-            <a href="#anchor-slide" title="P3ople" class="h2">Who we are</a>
+            <a href="#anchor-slide" title="Who we are" class="h2">Who we are</a>
             <p style="text-align: justify;">
               <?php
               $title = $conn->quote("WHO WE ARE");
@@ -56,7 +56,7 @@ function getDescription($conn, $title){
 <section class="container p-center fade-hidden" id="vision">
     <div class="row">
         <div class="col-xs-12 m-t-50 t-center">
-            <a href="#vision" title="P3ople" class="h2">Vision</a>
+            <a href="#vision" title="Vision" class="h2">Vision</a>
             <p>
               <?php
               $title = $conn->quote("VISION");
@@ -70,10 +70,38 @@ function getDescription($conn, $title){
 <section class="container p-center fade-hidden" id="mission">
     <div class="row">
         <div class="col-xs-12 m-t-50 t-center">
-            <a href="#mission" title="P3ople" class="h2">Mission</a>
+            <a href="#mission" title="Mission" class="h2">Mission</a>
             <p>
               <?php
               $title = $conn->quote("MISSION");
+              echo getDescription($conn, $title);
+              ?>
+          </p>
+      </div>
+  </div>
+</section>
+
+<section class="container p-center fade-hidden" id="ourvalues">
+    <div class="row">
+        <div class="col-xs-12 m-t-50 t-center">
+            <a href="#ourvalues" title="Our values" class="h2">Our values</a>
+            <p>
+              <?php
+              $title = $conn->quote("OUR VALUES");
+              echo getDescription($conn, $title);
+              ?>
+          </p>
+      </div>
+  </div>
+</section>
+
+<section class="container p-center fade-hidden" id="legality">
+    <div class="row">
+        <div class="col-xs-12 m-t-50 t-center">
+            <a href="#legality" title="Legality" class="h2">Legality</a>
+            <p style="text-align: justify;">
+              <?php
+              $title = $conn->quote("LEGALITY");
               echo getDescription($conn, $title);
               ?>
           </p>
@@ -1509,167 +1537,170 @@ function getDescription($conn, $title){
     </div>
 </section>
 
-<section class="container hp-awards p-center fade-hidden">
+<section class="container hp-awards p-center fade-hidden" id="awards">
     <div class="row">
-      <div class="col-xs-12 m-b-35 m-t-50 t-center">
-         <a href="https://www.p3parks.com/en/company/awards/" title="Awards" class="h2">Awards</a>
-         <p>
-          Over the years, P3 has won a number of awards for everything from Best Logistics Park to CEE Investor of the Year to Industrial
-          Developer of the Year.
-      </p>
-  </div>
-  <div class="col-xs-6 col-md-3 m-b-10">
-    <img src="https://c.p3parks.com/images/company_awards_1.png">
-</div>
-<div class="col-xs-6 col-md-3 m-b-10">
-    <img src="https://c.p3parks.com/images/award-see-real.png">
-</div>
-<div class="col-xs-6 col-md-3 m-b-10">
-    <img src="https://c.p3parks.com/images/awards-ceeqa.png">
-</div>
-<div class="col-xs-6 col-md-3 m-b-10">
-    <img src="https://c.p3parks.com/images/awards-cij.png">
-</div>
-<div class="col-xs-12 m-t-55 t-center"> <a href="https://www.p3parks.com/en/company/awards/" title="Awards" class="btn btn-default">SHOW ALL</a></div>
-</div>
+        <div class="col-xs-12 m-b-35 m-t-50 t-center">
+            <a href="#awards" title="Awards" class="h2">Awards</a>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+        </div>
+        <div class="col-xs-6 col-md-3 m-b-10">
+            <img src="https://c.p3parks.com/images/company_awards_1.png">
+        </div>
+        <div class="col-xs-6 col-md-3 m-b-10">
+            <img src="https://c.p3parks.com/images/award-see-real.png">
+        </div>
+        <div class="col-xs-6 col-md-3 m-b-10">
+            <img src="https://c.p3parks.com/images/awards-ceeqa.png">
+        </div>
+        <div class="col-xs-6 col-md-3 m-b-10">
+            <img src="https://c.p3parks.com/images/awards-cij.png">
+        </div>
+        <div class="col-xs-12 m-t-55 t-center"> <a href="#" title="Awards" class="btn btn-default">SHOW ALL</a></div>
+        <!-- <div class="col-xs-12 m-t-55 t-center"> <a href="https://www.p3parks.com/en/company/awards/" title="Awards" class="btn btn-default">SHOW ALL</a></div> -->
+    </div>
 </section>
 
-<section class="container p-center fade-hidden">
+<!-- <section class="container p-center fade-hidden">
     <div class="row">
-      <div class="col-xs-12 m-t-50 t-center">
-         <a href="https://www.p3parks.com/en/company/careers/" title="Careers" class="h2">Careers</a>
-         <p>
-          We´re always looking for talented and motivated people to join our dedicated team of specialists.
-      </p>
-  </div>
-</div>
-</section>
+        <div class="col-xs-12 m-t-50 t-center">
+            <a href="https://www.p3parks.com/en/company/careers/" title="Careers" class="h2">Careers</a>
+            <p>
+                We´re always looking for talented and motivated people to join our dedicated team of specialists.
+            </p>
+        </div>
+    </div>
+</section> -->
 
-<section class="container-fluid bg-lightgray m-t-20 fade-hidden">
+<!-- <section class="container-fluid bg-lightgray m-t-20 fade-hidden">
     <div class="row parallaxParent" id="parallax2">
-      <div class="filter" style="transform: matrix(1, 0, 0, 1, 0, 0);"></div>
-      <div class="center-box-mob" style="transform: matrix(1, 0, 0, 1, -573.5, -58.5);">
-        <h4>Do you love warehouses as much as we do?</h4>
-        <a href="https://www.p3parks.com/en/company/careers/" title="Careers" class="btn btn-default">SHOW VACANCIES</a>
+        <div class="filter" style="transform: matrix(1, 0, 0, 1, 0, 0);"></div>
+        <div class="center-box-mob" style="transform: matrix(1, 0, 0, 1, -573.5, -58.5);">
+            <h4>Do you love warehouses as much as we do?</h4>
+            <a href="https://www.p3parks.com/en/company/careers/" title="Careers" class="btn btn-default">SHOW VACANCIES</a>
+        </div>
+        <div class="col-xs-12" style="background-image: url(&quot;https://c.p3parks.com/images/company_careers.png&quot;); transform: matrix(1, 0, 0, 1, 0, 0);"></div>
     </div>
-    <div class="col-xs-12" style="background-image: url(&quot;https://c.p3parks.com/images/company_careers.png&quot;); transform: matrix(1, 0, 0, 1, 0, 0);"></div>
-</div>
+</section> -->
 
-</section>
-
-<section class="container p-center fade-hidden">
+<!-- <section class="container p-center fade-hidden">
     <div class="row">
-      <div class="col-xs-12 m-t-50 t-center">
-         <a href="https://www.p3parks.com/en/company/contacts/" title="Contacts" class="h2">Contact</a>
-         <p>
-          From Bucharest to Madrid, Warsaw to Milan, we offer facilities and cross-border services in 11 European countries.
-      </p>
-  </div>
-</div>
-</section>
-
-<section class="container-fluid m-t-20 fade-hidden">
-    <div class="row parallaxParent" id="parallax1">
-      <div class="filter" style="transform: matrix(1, 0, 0, 1, 0, 0);"></div>
-      <div class="center-box-mob" style="transform: matrix(1, 0, 0, 1, -573.5, -76.5);">
-        <h4>BULGARIA. CZECH REPUBLIC. FRANCE. GERMANY. ITALY. NETHERLANDS. POLAND. ROMANIA. SERBIA. SLOVAKIA. SPAIN.</h4>
-        <a href="https://www.p3parks.com/en/company/contacts/" title="Contacts" class="btn btn-default">Show all Contacts</a>
+        <div class="col-xs-12 m-t-50 t-center">
+            <a href="https://www.p3parks.com/en/company/contacts/" title="Contacts" class="h2">Contact</a>
+            <p>
+                From Bucharest to Madrid, Warsaw to Milan, we offer facilities and cross-border services in 11 European countries.
+            </p>
+        </div>
     </div>
-    <div class="col-xs-12" style="background-image: url(&quot;https://c.p3parks.com/images/company_contact.png&quot;); transform: matrix(1, 0, 0, 1, 0, 0);"></div>
-</div>
-</section>
+</section> -->
+
+<!-- <section class="container-fluid m-t-20 fade-hidden">
+    <div class="row parallaxParent" id="parallax1">
+        <div class="filter" style="transform: matrix(1, 0, 0, 1, 0, 0);"></div>
+        <div class="center-box-mob" style="transform: matrix(1, 0, 0, 1, -573.5, -76.5);">
+            <h4>BULGARIA. CZECH REPUBLIC. FRANCE. GERMANY. ITALY. NETHERLANDS. POLAND. ROMANIA. SERBIA. SLOVAKIA. SPAIN.</h4>
+            <a href="https://www.p3parks.com/en/company/contacts/" title="Contacts" class="btn btn-default">Show all Contacts</a>
+        </div>
+        <div class="col-xs-12" style="background-image: url(&quot;https://c.p3parks.com/images/company_contact.png&quot;); transform: matrix(1, 0, 0, 1, 0, 0);"></div>
+    </div>
+</section> -->
 
 <section class="container-fluid m-b-70 subscribe fade-hidden">
     <div class="container">
-      <div class="row">
-        <div class="col-xs-12 t-center m-t-50">
-          <h2 class="h2-blue">P3 NEWS<span class="t-red-dot">.</span></h2>
-          <p class="m-b-15" style="font-size: 3.0rem;">Recieve the latest news straight to your inbox!</p>
+        <div class="row">
+            <div class="col-xs-12 t-center m-t-50">
+                <h2 class="h2-blue">PRP<span class="t-red-dot">.</span></h2>
+                <p class="m-b-15" style="font-size: 3.0rem;">Recieve the latest news straight to your inbox!</p>
 
-          <script type="text/javascript">
-            function fbht(htid)
-            {
-                var fbhtc=document.getElementById(htid);
-                if (fbhtc)
-                {
-                  if (fbhtc.style.display == 'none')
-                  {
-                    fbhtc.style.display = 'inline';
-                }
-                else
-                {
-                    fbhtc.style.display = 'none';
-                }
-            }
-        }
-
-
-    </script>
+                <script type="text/javascript">
+                    function fbht(htid)
+                    {
+                        var fbhtc=document.getElementById(htid);
+                        if (fbhtc)
+                        {
+                            if (fbhtc.style.display == 'none')
+                            {
+                                fbhtc.style.display = 'inline';
+                            }
+                            else
+                            {
+                                fbhtc.style.display = 'none';
+                            }
+                        }
+                    }
 
 
-    <!-- Start FormBuilder Module (1.0) -->
+                </script>
 
 
-    <form id="m2b7d3moduleform_1" method="post" action="https://www.p3parks.com/en/company/" class="cms_form" enctype="multipart/form-data">
-        <div class="hidden">
-            <input type="hidden" name="mact" value="FormBuilder,m2b7d3,default,1">
-            <input type="hidden" name="m2b7d3fbrp_callcount" value="1">
-        </div>
+                <!-- Start FormBuilder Module (1.0) -->
+                <!-- https://www.p3parks.com/en/company/ -->
+                <form id="m2b7d3moduleform_1" method="post" action="" class="cms_form" enctype="multipart/form-data">
+                    <div class="hidden">
+                        <input type="hidden" name="mact" value="FormBuilder,m2b7d3,default,1">
+                        <input type="hidden" name="m2b7d3fbrp_callcount" value="1">
+                    </div>
 
-        <div><input type="hidden" id="m2b7d3form_id" name="m2b7d3form_id" value="1">
-            <input type="hidden" id="m2b7d3lang" name="m2b7d3lang" value="en_US">
-            <input type="hidden" id="m2b7d3fbrp_continue" name="m2b7d3fbrp_continue" value="2">
-            <input type="hidden" id="m2b7d3fbrp_done" name="m2b7d3fbrp_done" value="1">
-        </div>
-        <div class="contact-form">
-            <div data-lang="Enter email address" class="required input-min-height replaceLang"><input type="text" name="m2b7d3fbrp__21" value="" size="25" maxlength="128" onfocus="if(this.value==this.defaultValue) this.value='';" onblur="if(this.value=='') this.value=this.defaultValue;" id="fbrp__21">
+                    <div><input type="hidden" id="m2b7d3form_id" name="m2b7d3form_id" value="1">
+                        <input type="hidden" id="m2b7d3lang" name="m2b7d3lang" value="en_US">
+                        <input type="hidden" id="m2b7d3fbrp_continue" name="m2b7d3fbrp_continue" value="2">
+                        <input type="hidden" id="m2b7d3fbrp_done" name="m2b7d3fbrp_done" value="1">
+                    </div>
+                    <div class="contact-form">
+                        <div data-lang="Enter email address" class="required input-min-height replaceLang"><input type="text" name="m2b7d3fbrp__21" value="" size="25" maxlength="128" onfocus="if(this.value==this.defaultValue) this.value='';" onblur="if(this.value=='') this.value=this.defaultValue;" id="fbrp__21">
+                        </div>
+
+                        <div class="submit" data-lang="SUBSCRIBE"><input class="cms_submit fbsubmit" name="m2b7d3fbrp_submit" id="m2b7d3fbrp_submit" value="SUBSCRIBE" type="submit"></div>
+                    </div>
+                </form>
+
+
+                <!-- End FormBuilder Module -->
+
+
+
+                <script type="text/javascript">
+
+                    $(function() {
+                        var langReplace = $(".submit").data("lang");
+                        var langInput = $(".replaceLang").data("lang");
+                        $(".submit input").val(langReplace);
+                        $(".replaceLang input").val(langInput);
+
+                        $(".replaceLang input:text").each(function ()
+                        {
+// store default value
+var v = this.value;
+
+$(this).blur(function ()
+{
+// if input is empty, reset value to default
+if (this.value.length == 0) this.value = langInput;
+}).focus(function ()
+{
+// when input is focused, clear its contents
+this.value = "";
+});
+});
+
+                        var inputVal = $("#fbrp__21").val();
+                        var inputCount = inputVal.length;
+
+                        if (inputCount >= 43) {
+                            $("#fbrp__21").css("width", "480px");
+                        }
+                    });
+
+                </script>
             </div>
-
-            <div class="submit" data-lang="SUBSCRIBE"><input class="cms_submit fbsubmit" name="m2b7d3fbrp_submit" id="m2b7d3fbrp_submit" value="SUBSCRIBE" type="submit"></div>
         </div>
-    </form>
-
-
-    <!-- End FormBuilder Module -->
-
-
-
-    <script type="text/javascript">
-
-        $(function() {
-            var langReplace = $(".submit").data("lang");
-            var langInput = $(".replaceLang").data("lang");
-            $(".submit input").val(langReplace);
-            $(".replaceLang input").val(langInput);
-
-            $(".replaceLang input:text").each(function ()
-            {
-                // store default value
-                var v = this.value;
-
-                $(this).blur(function ()
-                {
-                    // if input is empty, reset value to default
-                    if (this.value.length == 0) this.value = langInput;
-                }).focus(function ()
-                {
-                    // when input is focused, clear its contents
-                    this.value = "";
-                });
-            });
-
-            var inputVal = $("#fbrp__21").val();
-            var inputCount = inputVal.length;
-
-            if (inputCount >= 43) {
-              $("#fbrp__21").css("width", "480px");
-          }
-      });
-
-  </script>
-</div>
-</div>
-</div>
+    </div>
 </section>
 <!-- /content -->
 </main>
