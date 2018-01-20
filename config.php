3d85ddd516f65a4b9e8fb8fde1af3a19b7d82055
malikkhoiri <abdulchoiri19@gmail.com>
2018-01-20 12:54:20 +0700

    add connection

--- config.php
+++ config.php
@@ -1,2 +1,13 @@
-<?php  ?>
-umar
+<?php  
+	$host = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "dbprp";
+
+	try {
+		$conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	} catch (Exception $e) {
+		echo "Connection failed: ".$e->getMessage();
+	}
+?>
