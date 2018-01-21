b65550d80b6f1c8d5b35cee80a7ea16161f54fd3
umarkotak <umarkotak@yahoo.co.id>
2018-01-21 12:31:30 +0700

    creating global footer

--- pages/home.php
+++ pages/home.php
@@ -118,17 +118,6 @@
   <a id="cookies-rules-box-btn" href="#">CONFIRM</a>
 </div>
 
-<footer class="container-fluid op-0">
-  <div class="row">
-    <div class="col-xs-12 col-sm-12">
-      <p style="font-size: 15px; color: #002699; text-align: center;"><b>Copyright by : </b>PT Presisi Rekayasa | 2018</p>
-    </div>
-    <div class="col-xs-12 col-sm-12 t-right">
-
-    </div>
-  </div>
-</footer>
-
 <!-- SPLASH LOGO -->
 <!-- <div id="loader"></div> -->
 
