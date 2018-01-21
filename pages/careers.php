88681db2c508e42946857ce299c12ccc30312b1b
malikkhoiri <abdulchoiri19@gmail.com>
2018-01-21 12:29:00 +0700

    edit careers

--- pages/careers.php
+++ pages/careers.php
@@ -1,231 +1,46 @@
+<?php  
+function getDescription($conn, $title){
+  $sql = $conn->prepare("SELECT ftp_description FROM fourth_page WHERE ftp_title=$title LIMIT 1");
+  $sql->execute();
+  $data = $sql->fetch();
+  return $data['ftp_description'];
+}
+?>
+
 <main class="op-0" style="min-height: calc(100vh - 78px); opacity: 1;">
-                    <div id="sub-nav">
-                <span class="lastitem">P3 Stories</span>
-            </div>
-                <!-- content -->
-<section class="container-fluid p3stories fade-hidden fade-visible animated fadeIn">
-    <div class="row">
-    <div class="col-xs-12 col-md-6 p-0">
-        <div class="bx-wrapper bx-wrapper-image-small bx-wrapper-image-small-large" style="max-width: 100%; margin: 0px auto;"><div class="bx-viewport" style="width: 100%; overflow: hidden; position: relative; height: 319px;"><ul class="slider-image-small large-slider" style="width: auto; position: relative;">
-
-            <li data-src="https://c.p3parks.com/images/case-study/case_study_344_MD-Logistika/MD1.jpg" style="float: none; list-style: none; position: absolute; display: block; background: url(&quot;https://c.p3parks.com/images/case-study/case_study_344_MD-Logistika/MD1.jpg&quot;); width: 819px; z-index: 50;" class="loaded">
-                <a href="https://www.p3parks.com/case-studies" class="p3-stories-link">
-              <div class="filter"></div>
-              <div class="center-box">
-                  <h3>Case Studies<span class="dot-red"></span></h3>
-                    </div>
-                    </a>
-                    <div class="bottom-box">
-                        <div class="middle-box">
-                            <p>A market leader in FMCG storage &amp; logistics in the Czech Republic</p>
-                            <div class="white-line"></div>
-                        </div>
-                        <a href="https://www.p3parks.com/en/our-stories/case-study/344-md-logistika/">
-                      <h4>MD Logistika</h4>
-                  </a>
-                    </div>
-            </li>
-
-
-
-            <li data-src="https://c.p3parks.com/images/uvod_box_left_1.png" style="float: none; list-style: none; position: absolute; background: url(&quot;https://c.p3parks.com/images/uvod_box_left_1.png&quot;); display: none; z-index: 0; width: 819px;" class="loaded">
-                <a href="https://www.p3parks.com/case-studies" class="p3-stories-link">
-              <div class="filter"></div>
-              <div class="center-box">
-                  <h3>Case studies <span class="dot-red"></span></h3>
-                    </div>
-                    </a>
-                    <div class="bottom-box">
-                        <div class="middle-box">
-                            <p>Producing 50,000 Sofas per year. Saving 30% on warehouse construction and operational costs</p>
-                            <div class="white-line"></div>
-                        </div>
-                        <a href="https://www.p3parks.com/en/our-stories/case-study/7-sofa-com/">
-                      <h4>Sofa.com</h4>
-                  </a>
-                    </div>
-            </li>
-            <li data-src="https://c.p3parks.com/images/case-study/DHL_D8_005.jpg" style="float: none; list-style: none; position: absolute; background: url(&quot;https://c.p3parks.com/images/case-study/DHL_D8_005.jpg&quot;); display: none; z-index: 0; width: 819px;" class="loaded">
-                <a href="https://www.p3parks.com/case-studies" class="p3-stories-link">
-              <div class="filter"></div>
-              <div class="center-box">
-                  <h3>Case studies <span class="dot-red"></span></h3>
-                    </div>
-                    </a>
-                    <div class="bottom-box">
-                        <div class="middle-box">
-                            <p>€2.6 MILLION INVESTMENT BY DHL. 120 PEOPLE WORK IN THE BUILDING</p>
-                            <div class="white-line"></div>
-                        </div>
-                        <a href="https://www.p3parks.com/en/our-stories/case-study/53-dhl/">
-                      <h4>DHL</h4>
-                  </a>
-                    </div>
-            </li>
-                        <li data-src="https://c.p3parks.com/images/case-study/case_study_179-Yusen/D1_Yusen_026.jpg" style="float: none; list-style: none; position: absolute; background: url(&quot;https://c.p3parks.com/images/case-study/case_study_179-Yusen/D1_Yusen_026.jpg&quot;); display: none; z-index: 0; width: 819px;" class="loaded">
-                <a href="https://www.p3parks.com/case-studies" class="p3-stories-link">
-              <div class="filter"></div>
-              <div class="center-box">
-                  <h3>Case studies <span class="dot-red"></span></h3>
-                    </div>
-                    </a>
-                    <div class="bottom-box">
-                        <div class="middle-box">
-                            <p>NEW 25,000 M<sup>2</sup> CZECH HEADQUARTERS. 45% ENERGY SAVINGS &amp; REAL-TIME TRACKING.</p>
-                            <div class="white-line"></div>
-                        </div>
-                        <a href="https://www.p3parks.com/en/our-stories/case-study/179-yusen/">
-                      <h4>YUSEN LOGISTICS</h4>
-                  </a>
-                    </div>
-            </li>
-            <li data-src="https://c.p3parks.com/images/case-study/case_study_52-ID-LOGISTICS/IDLweek33-322-1.jpg" style="float: none; list-style: none; position: absolute; background: url(&quot;https://c.p3parks.com/images/case-study/case_study_52-ID-LOGISTICS/IDLweek33-322-1.jpg&quot;); display: none; z-index: 0; width: 819px;" class="loaded">
-                <a href="https://www.p3parks.com/case-studies" class="p3-stories-link">
-              <div class="filter"></div>
-              <div class="center-box">
-                  <h3>Case studies <span class="dot-red"></span></h3>
-                    </div>
-                    </a>
-                    <div class="bottom-box">
-                        <div class="middle-box">
-                            <p>52 MILLION CONSIGNMENTS PER YEAR. ONE OF THE LARGEST LOGISTICS HUBS IN POLAND</p>
-                            <div class="white-line"></div>
-                        </div>
-                        <a href="https://www.p3parks.com/en/our-stories/case-study/181-id-logistics/">
-                      <h4>ID LOGISTICS</h4>
-                  </a>
-                    </div>
-            </li>
-            <li data-src="https://c.p3parks.com/images/case-study/case_study_6-VZV/030_DSC9904.jpg" style="float: none; list-style: none; position: absolute; background: url(&quot;https://c.p3parks.com/images/case-study/case_study_6-VZV/030_DSC9904.jpg&quot;); display: none; z-index: 0; width: 819px;" class="loaded">
-                <a href="https://www.p3parks.com/case-studies" class="p3-stories-link">
-              <div class="filter"></div>
-              <div class="center-box">
-                  <h3>Case studies <span class="dot-red"></span></h3>
-                    </div>
-                    </a>
-                    <div class="bottom-box">
-                        <div class="middle-box">
-                            <p>FORKLIFTS FROM A P3 PARK ARE CONQUERING THE WORLD</p>
-                            <div class="white-line"></div>
-                        </div>
-                        <a href="https://www.p3parks.com/en/our-stories/case-study/6-project-name/">
-                      <h4>VZV</h4>
-                  </a>
-                    </div>
-            </li>
-              <li data-src="https://c.p3parks.com/images/case-study/case_study_8-RAJAPACK/u_MG_7986.jpg" style="float: none; list-style: none; position: absolute; background: url(&quot;https://c.p3parks.com/images/case-study/case_study_8-RAJAPACK/u_MG_7986.jpg&quot;); display: none; width: 819px; z-index: 0;" class="loaded">
-                <a href="https://www.p3parks.com/case-studies" class="p3-stories-link">
-              <div class="filter"></div>
-              <div class="center-box">
-                  <h3>Case studies <span class="dot-red"></span></h3>
-                    </div>
-                    </a>
-                    <div class="bottom-box">
-                        <div class="middle-box">
-                            <p>PACKING FOR 500,000 CUSTOMERS IN EUROPE</p>
-                            <div class="white-line"></div>
-                        </div>
-                        <a href="https://www.p3parks.com/en/our-stories/case-study/8-rajapack/">
-                      <h4>RAJAPACK</h4>
-                  </a>
-                    </div>
-            </li>
-          </ul></div><div class="bx-controls bx-has-controls-direction"><div class="bx-controls-direction"><a class="bx-prev" href="">Prev</a><a class="bx-next" href="">Next</a></div></div></div>
-    </div>
-    <div class="col-xs-12 col-md-6 p-0">
-        <div class="bx-wrapper bx-wrapper-image-small-2 bx-wrapper-image-small-large" style="max-width: 100%; margin: 0px auto;"><div class="bx-viewport" style="width: 100%; overflow: hidden; position: relative; height: 319px;"><ul class="slider-image-small-2 large-slider" style="width: auto; position: relative;">
-            <li data-src="https://c.p3parks.com/images/customer-stories/customer_stories_317_InPost/01Inpost_cover.jpg" style="float: none; list-style: none; position: absolute; display: none; background: url(&quot;https://c.p3parks.com/images/customer-stories/customer_stories_317_InPost/01Inpost_cover.jpg&quot;); z-index: 0; width: 819px;" class="loaded">
-                <a href="https://www.p3parks.com/customer-stories" class="customer-stories-link">
-              <div class="filter"></div>
-              <div class="center-box">
-                  <h3>Customer stories <span class="dot-red"></span></h3>
-                    </div>
-                    </a>
-                    <div class="bottom-box">
-                        <div class="middle-box">
-                            <p>How Paczkomat parcel pick-up terminals won the hearts of Poles</p>
-                            <div class="white-line"></div>
-                        </div>
-                        <a href="https://www.p3parks.com/en/our-stories/customer-stories/317-inpost/">
-                      <h4>InPost</h4>
-                  </a>
-                    </div>
-            </li>
-
-            <li data-src="https://c.p3parks.com/images/case-study/case_study_344_MD-Logistika/MD43.jpg" style="float: none; list-style: none; position: absolute; background: url(&quot;https://c.p3parks.com/images/case-study/case_study_344_MD-Logistika/MD43.jpg&quot;); display: none; width: 819px; z-index: 0;" class="loaded">
-                    <a href="https://www.p3parks.com/customer-stories" class="customer-stories-link">
-                        <div class="filter"></div>
-                        <div class="center-box">
-                            <h3>
-
-
-Customer stories<span class="dot-red"></span>
-
-
-                            </h3>
-                        </div>
-                    </a>
-                    <div class="bottom-box">
-                        <div class="middle-box">
-                            <p>
-
-
-A Kingdom of pallets
-
-
-                            </p>
-                            <div class="white-line"></div>
-                        </div>
-                        <a href="https://www.p3parks.com/en/our-stories/customer-stories/346-md-logistika/">
-                            <h4>
-
-
-MD LOGISTIKA
-
-
-                            </h4>
-                        </a>
-                    </div>
-                </li>
-
-
-            <li data-src="https://c.p3parks.com/images/customer-stories/customer_stories_176-Muziker/_DSC3230.jpg" style="float: none; list-style: none; position: absolute; background: url(&quot;https://c.p3parks.com/images/customer-stories/customer_stories_176-Muziker/_DSC3230.jpg&quot;); display: list-item; width: 819px; z-index: 50;" class="loaded">
-                <a href="https://www.p3parks.com/customer-stories" class="customer-stories-link">
-              <div class="filter"></div>
-              <div class="center-box">
-                  <h3>Customer stories <span class="dot-red"></span></h3>
-                    </div>
-                    </a>
-                    <div class="bottom-box">
-                        <div class="middle-box">
-                            <p>HALF OF EUROPE BUYS GUITARS, RECORDERS AND PIANOS FROM MUZIKER</p>
-                            <div class="white-line"></div>
-                        </div>
-                       <a href="https://www.p3parks.com/en/our-stories/customer-stories/180-muziker/">
-                      <h4>MUZIKER</h4>
-                  </a>
-                    </div>
-            </li>
-            <li data-src="https://c.p3parks.com/images/case-study/case_study_6-VZV/030_DSC9904.jpg" style="float: none; list-style: none; position: absolute; background: url(&quot;https://c.p3parks.com/images/case-study/case_study_6-VZV/030_DSC9904.jpg&quot;); display: none; z-index: 0; width: 819px;" class="loaded">
-                <a href="https://www.p3parks.com/customer-stories" class="customer-stories-link">
-              <div class="filter"></div>
-              <div class="center-box">
-                  <h3>Customer stories <span class="dot-red"></span></h3>
-                    </div>
-                    </a>
-                    <div class="bottom-box">
-                        <div class="middle-box">
-                            <p>FORKLIFTS FROM A P3 PARK ARE CONQUERING THE WORLD</p>
-                            <div class="white-line"></div>
-                        </div>
-                       <a href="https://www.p3parks.com/en/our-stories/customer-stories/178-vzv/">
-                      <h4>VZV</h4>
-                  </a>
-                    </div>
-            </li>
-          </ul></div><div class="bx-controls bx-has-controls-direction"><div class="bx-controls-direction"><a class="bx-prev" href="">Prev</a><a class="bx-next" href="">Next</a></div></div></div>
-    </div>
+  <div id="sub-nav">
+    <span class="lastitem">Careers</span>
   </div>
-</section>
-<!-- /content -->
-    </main>
+  <!-- content -->
+  <section class="container-fluid fade-hidden fade-visible animated fadeIn full-visible">
+    <div class="row">
+      <div class="image-cover loaded" data-src="https://c.p3parks.com/images/company_1.png" style="max-height: 537px; background: url(&quot;https://c.p3parks.com/images/company_1.png&quot;);">
+        <div class="filter"></div>
+
+        <div class="center-box">
+          <a href="#" title="Grow with Presisi" class="h1">Grow with Presisi</a>
+        </div>
+
+        <div class="container link-box-bottom">
+          <div class="row">
+            <a class="company-arrow" href="#" id="hp-arrow"><img src="https://c.p3parks.com/images/design/2016/hp-arrow.png" width="80" height="41" alt="[design/2016/hp-arrow.png]"></a></div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="container p-center fade-hidden" id="anchor-slide">
+      <div class="row">
+        <div class="col-xs-12 m-t-50 t-center">
+          <a href="#anchor-slide" title="Careers" class="h2">Careers</a>
+          <p style="text-align: justify;">
+            <?php
+            $title = $conn->quote("Grow with Presisi");
+            echo getDescription($conn, $title);
+            ?>
+          </p>
+        </div>
+      </div>
+    </section>
+  <!-- /content -->
+</main>
