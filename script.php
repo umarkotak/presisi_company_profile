1492d47da982724d68fb13f71e5da15bf3c5edf3
umarkotak <umarkotak@yahoo.co.id>
2018-01-27 19:58:25 +0700

    working on css and js

--- script.php
+++ script.php
@@ -7,22 +7,22 @@
   $zoho.salesiq.ready=function(embedinfo){$zoho.salesiq.floatbutton.visible("hide");}
 </script>
 
-<script type="text/javascript">
+<!-- <script type="text/javascript">
   var $zoho= $zoho || {salesiq:{values:{},ready:function(){}}};var d=document;s=d.createElement("script");s.type="text/javascript";
   s.defer=true;s.src="style/js4.js";
   t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);
-</script>
+</script> -->
 
-<script type="text/javascript">
+<!-- <script type="text/javascript">
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
   ga('create', 'UA-26369507-1', 'auto');
   ga('send', 'pageview');
-</script>
+</script> -->
 
-<script type="text/javascript">
+<!-- <script type="text/javascript">
   var leady_track_key="XSnB2eIcJpnvA8mH";
   var leady_track_server=document.location.protocol+"//t.leady.cz/";
   (function(){
@@ -30,4 +30,4 @@
     l.src=leady_track_server+leady_track_key+"/L.js";
     var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(l,s);
   })();
-</script>
+</script> -->
