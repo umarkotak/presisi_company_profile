824dffbb3d858ba7de19817a3b81b2d01a7f4308
umarkotak <umarkotak@yahoo.co.id>
2018-02-04 15:40:38 +0700

    add minor border on company

--- pages/company.php
+++ pages/company.php
@@ -1,119 +1,120 @@
 <?php
 function getDescription($conn, $title){
-    $sql = $conn->prepare("SELECT sp_description FROM second_page WHERE sp_title=$title LIMIT 1");
-    $sql->execute();
-    $data = $sql->fetch();
-    return $data['sp_description'];
+  $sql = $conn->prepare("SELECT sp_description FROM second_page WHERE sp_title=$title LIMIT 1");
+  $sql->execute();
+  $data = $sql->fetch();
+  return $data['sp_description'];
 }
 ?>
 
 <main class="op-0" style="min-height: calc(100vh - 78px); opacity: 1;">
-<div id="sub-nav">
+  <div id="sub-nav">
     <span class="lastitem">Company</span>
-</div>
-<!-- content -->
-<section class="container-fluid fade-hidden fade-visible animated fadeIn full-visible">
+  </div>
+  <!-- content -->
+  <section class="container-fluid fade-hidden fade-visible animated fadeIn full-visible">
     <div class="row">
-        <div class="image-cover loaded" data-src="http://localhost/presisi_company_profile/images/company_1.png" style="max-height: 537px; background: url(&quot;http://localhost/presisi_company_profile/images/company_1.png&quot;);">
-            <div class="filter"></div>
+      <div class="image-cover loaded" data-src="http://localhost/presisi_company_profile/images/company_1.png" style="max-height: 537px; background: url(&quot;http://localhost/presisi_company_profile/images/company_1.png&quot;);">
+        <div class="filter"></div>
 
-            <div class="center-box">
-                <a href="#" title="Our Story" class="h1">Our Company</a>
-                <p>
-                    Lorem ipsum
-                </p>
-            </div>
-
-            <div class="container link-box-bottom">
-                <div class="row">
-                    <div class="col-xs-12 t-center company-buttons">
-                        <div class="col-xs-12 col-md-one-fifth"> <a href="#anchor-slide" title="Who we are" class="btn btn-default">Who we are</a></div>
-                        <div class="col-xs-12 col-md-one-fifth"> <a href="#vision" title="Vision & Mission" class="btn btn-default">Vision & Mission</a></div>
-                        <div class="col-xs-12 col-md-one-fifth"> <a href="#ourvalues" title="Our values" class="btn btn-default">Our values</a></div>
-                        <div class="col-xs-12 col-md-one-fifth"> <a href="#legality" title="Legality" class="btn btn-default">Legality</a></div>
-                        <div class="col-xs-12 col-md-one-fifth"> <a href="#awards" title="Legality" class="btn btn-default">Awards</a></div>
-                    </div>
-                    <a class="company-arrow" href="#" id="hp-arrow"><img src="https://c.p3parks.com/images/design/2016/hp-arrow.png" width="80" height="41" alt="[design/2016/hp-arrow.png]"></a></div>
-                </div>
-            </div>
+        <div class="center-box">
+          <a href="#" title="Our Story" class="h1">Our Company</a>
+          <p>
+            Lorem ipsum
+          </p>
         </div>
-</section>
 
-<section class="container p-center fade-hidden" id="anchor-slide" style="border-bottom: solid 1px #b3d9ff;">
-    <div class="row">
-      <div class="col-xs-12 m-t-50 t-center">
-        <a href="#anchor-slide" title="Who we are" class="h2">Who we are</a>
-        <p style="text-align: justify;">
-          <?php
-          $title = $conn->quote("WHO WE ARE");
-          echo getDescription($conn, $title);
-          ?>
-        </p>
+        <div class="container link-box-bottom">
+          <div class="row">
+            <div class="col-xs-12 t-center company-buttons">
+              <div class="col-xs-12 col-md-one-fifth"> <a href="#anchor-slide" title="Who we are" class="btn btn-default">Who we are</a></div>
+              <div class="col-xs-12 col-md-one-fifth"> <a href="#vision" title="Vision & Mission" class="btn btn-default">Vision & Mission</a></div>
+              <div class="col-xs-12 col-md-one-fifth"> <a href="#ourvalues" title="Our values" class="btn btn-default">Our values</a></div>
+              <div class="col-xs-12 col-md-one-fifth"> <a href="#legality" title="Legality" class="btn btn-default">Legality</a></div>
+              <div class="col-xs-12 col-md-one-fifth"> <a href="#awards" title="Legality" class="btn btn-default">Awards</a></div>
+            </div>
+            <a class="company-arrow" href="#" id="hp-arrow"><img src="https://c.p3parks.com/images/design/2016/hp-arrow.png" width="80" height="41" alt="[design/2016/hp-arrow.png]"></a></div>
+          </div>
+        </div>
       </div>
-  </div>
-</section>
+    </section>
 
-<section class="container p-center fade-hidden" id="vision" style="border-bottom: solid 1px #b3d9ff;">
-  <div class="row">
-    <div class="col-xs-6 m-t-50 t-center">
-      <a href="#vision" title="Vision" class="h2"><i class="fa fa-dot-circle-o"></i>Vision</a>
-      <p>
-        <?php
-        $title = $conn->quote("VISION");
-        echo getDescription($conn, $title);
-        ?>
-      </p>
-    </div>
-
-    <div class="col-xs-6 m-t-50 t-center" id="mission">
-      <a href="#vision" title="Vision" class="h2"><i class="fa fa-check-circle-o"></i>Mission</a>
-      <p>
-        <?php
-        $title = $conn->quote("MISSION");
-        echo getDescription($conn, $title);
-        ?>
-      </p>
-    </div>
-  </div>
-
-  <br><br>
-</section>
-
-
-
-<section class="container p-center fade-hidden" id="ourvalues">
-    <div class="row">
+    <section class="container p-center fade-hidden" id="anchor-slide" style="border-bottom: solid 1px #b3d9ff;">
+      <div class="row">
         <div class="col-xs-12 m-t-50 t-center">
-            <a href="#ourvalues" title="Our values" class="h2">Our values</a>
-            <p>
-              <?php
-              $title = $conn->quote("OUR VALUES");
-              echo getDescription($conn, $title);
-              ?>
+          <a href="#anchor-slide" title="Who we are" class="h2">Who we are</a>
+          <p style="text-align: justify;">
+            <?php
+            $title = $conn->quote("WHO WE ARE");
+            echo getDescription($conn, $title);
+            ?>
           </p>
+        </div>
       </div>
-  </div>
-</section>
+    </section>
 
-<section class="container p-center fade-hidden" id="legality">
-    <div class="row">
+    <section class="container p-center fade-hidden" id="vision" style="border-bottom: solid 1px #b3d9ff;">
+      <div class="row">
+        <div class="col-xs-6 m-t-50 t-center">
+          <a href="#vision" title="Vision" class="h2"><i class="fa fa-dot-circle-o"></i>Vision</a>
+          <p>
+            <?php
+            $title = $conn->quote("VISION");
+            echo getDescription($conn, $title);
+            ?>
+          </p>
+        </div>
+
+        <div class="col-xs-6 m-t-50 t-center" id="mission">
+          <a href="#vision" title="Vision" class="h2"><i class="fa fa-check-circle-o"></i>Mission</a>
+          <p>
+            <?php
+            $title = $conn->quote("MISSION");
+            echo getDescription($conn, $title);
+            ?>
+          </p>
+        </div>
+      </div>
+
+      <br><br>
+    </section>
+
+
+
+    <section class="container p-center fade-hidden" id="ourvalues" style="border-bottom: solid 1px #b3d9ff;">
+      <div class="row">
         <div class="col-xs-12 m-t-50 t-center">
-            <a href="#legality" title="Legality" class="h2">Legality</a>
-            <p style="text-align: center;">
-              <?php
-              $title = $conn->quote("LEGALITY");
-              echo getDescription($conn, $title);
-              ?>
+          <a href="#ourvalues" title="Our values" class="h2">Our values</a>
+          <p>
+            <?php
+            $title = $conn->quote("OUR VALUES");
+            echo getDescription($conn, $title);
+            ?>
           </p>
+        </div>
       </div>
-  </div>
-  <br><br>
-</section>
+      <br><br>
+    </section>
 
-<!-- /content -->
-</main>
+    <section class="container p-center fade-hidden" id="legality">
+      <div class="row">
+        <div class="col-xs-12 m-t-50 t-center">
+          <a href="#legality" title="Legality" class="h2">Legality</a>
+          <p style="text-align: center;">
+            <?php
+            $title = $conn->quote("LEGALITY");
+            echo getDescription($conn, $title);
+            ?>
+          </p>
+        </div>
+      </div>
+      <br><br>
+    </section>
 
-<script>
+    <!-- /content -->
+  </main>
+
+  <script>
     $(document).ready(function(){
         // Add smooth scrolling to all links
         $("a").on('click', function(event) {
@@ -129,8 +130,8 @@ function getDescription($conn, $title){
                 $('html, body').animate({scrollTop: $(hash).offset().top}, 1500, function(){
                     // Add hash (#) to URL when done scrolling (default click behavior)
                     window.location.hash = hash;
-                });
+                  });
             } // End if
-        });
-    });
-</script>
+          });
+      });
+    </script>
