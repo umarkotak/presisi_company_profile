cd5766985bab256366f6fef58ee72ffedbdc6e47
malikkhoiri <abdulchoiri19@gmail.com>
2018-02-19 00:30:50 +0700

    change copyright

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer class="container-fluid op-0">
   <div class="row">
     <div class="col-xs-12 col-sm-12">
-      <p style="font-size: 15px; color: #002699; text-align: center;"><b>Copyright by : </b>PT Presisi Rekayasa | 2018</p>
+      <p style="font-size: 15px; color: #002699; text-align: center;"><b>Copyright &copy; </b>PT Presisi Rekayasa, 2018. All Rights Reserved.</p>
     </div>
     <div class="col-xs-12 col-sm-12 t-right">
 
